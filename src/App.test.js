abf960c81a0c2834d2361b4580e41ccd0dd25a4c
David Boros <101596357+davidus98@users.noreply.github.com>
2025-07-19 13:20:28 +0300

    Remove screenshot image

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders intro text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const introElement = screen.getByText(/software engineer/i);
+  expect(introElement).toBeInTheDocument();
 });
